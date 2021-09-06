85fed7b5ff56dc1d7435a7ec44066e95b4315424
Santiago Frega <fregasantiago@gmail.com>
2021-09-06 19:52:56 -0300

    crop

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 import face_recognition
 
+
 def show_webcam():
     cam = cv2.VideoCapture(0)
     if not (cam.isOpened()):
         print("Could not open video device")
         return
     var = True
+    x = 0
+    face_locations = []
     while True:
         _, img = cam.read()
         small_frame = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
@@ -18,13 +21,16 @@ def show_webcam():
                 right *=2
                 bottom *= 2
                 cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-
-                cv2.rectangle(img, (left, bottom - 20), (right, bottom), (0, 0, 255), cv2.FILLED)
-                cv2.putText(img, "hola", (left + 6, bottom - 6),cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
+                x = img[top:bottom, left:right]
+                cv2.putText(img, "human", (left + 6, bottom - 6),cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
     
-        cv2.imshow('cam', img)
+        cv2.imshow('cam', cv2.resize(img, (0, 0), fx=2, fy=2))
+        #if cv2.waitKey(1) == 27:  break
         if cv2.waitKey(1) == 27: 
-            break  # esc to quit
+            print("zuke")
+            print(x)
+            cv2.imwrite("CroppedImage.jpg", x)
+            break
         var != var
     cv2.destroyAllWindows()
 
